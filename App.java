fecffdf67019bbd8440f7f0cc9711260e316204d
racelpa <pauline.joyce.racelis@oocl.com>
2016-06-13 03:42:55 +0000

    test

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.mnl.app2.my_app_web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
